8665958e87a71637f9e00c5562f3197eba2ed99a
AlejandroBravo <alejandrobravotrabajo@gmail.com>
2018-05-14 18:08:07 +0200

    Escribir Formulario

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class RegForm(forms.Form):
-	nombre = forms.charfield(max_length=100)
-	edad = forms.interferfield()
+	nombre = forms.CharField(max_length=100)
+	edad = forms.IntegerField()
